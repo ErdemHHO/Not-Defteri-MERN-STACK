02214f73f68c7f48a1820065fb79004ab78c22c9
ErdemHHO <75128279+ErdemHHO@users.noreply.github.com>
2023-01-08 02:09:50 +0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import SignUpScreen from './screens/SignUpScreen';
 import Header from './components/Header';
 import './bootstrap.min.css'
+
 
 function App() {
   return (
@@ -12,7 +15,9 @@ function App() {
       <Header />
         <div className='pages'>
           <Routes>
-            <Route path="/" element={<HomeScreen/>}/>
+            <Route path="/" element={<HomeScreen/>} exach/>
+            <Route path="/login" element={<LoginScreen/>}/>
+            <Route path="/signup" element={<SignUpScreen/>}/>
           </Routes>
         </div>
       </BrowserRouter>
